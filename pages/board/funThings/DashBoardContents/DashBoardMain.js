999664255de69e104c1256b5d5edd6467d20acaa
kucne <kucnea@naver.com>
2023-11-20 11:23:24 +0900

    fix comment

--- pages/board/funThings/DashBoardContents/DashBoardMain.js
+++ pages/board/funThings/DashBoardContents/DashBoardMain.js
@@ -1,8 +1,8 @@
 export default function DashBoardMain(){
     return (
         <div>
-            <h1 className="text-3xl font-semibold">Welcome back!</h1>
-            <p className="mt-2 text-gray-600">Here's what's been happening since your last visit.</p>
+            <h1 className="text-3xl font-semibold">Welcome!</h1>
+            <p className="mt-2 text-gray-600">Introduce What is NEW and HOT!</p>
             <div className="grid gap-6 mt-6 grid-cols-2">
             <div className="p-6 bg-white rounded shadow">
                 <h2 className="text-xl font-semibold">New Customers</h2>
