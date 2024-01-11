a6d6568d75c9d9a2fd1cf45b4c0beb556516ab05
kucne <kucnea@naver.com>
2024-01-11 23:09:54 +0900

    add Index Gradation

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ import kakao from '/public/image/kakao.png'
 
 export default function Index() {
 
+  const [isMobile,setIsMobile] = useState(false);
+  const mobileWidth = 768;
   const [currentImage, setCurrentImage] = useState(0);
   const [currentComponent, setCurrentComponent] = useState(0);
 
@@ -35,7 +37,6 @@ export default function Index() {
       window.location.href = '/board/funThings/DashBoard'
     }
     setCurrentComponent(i);
-    console.log(currentComponent);
   }
 
   function SelectComponent(){
@@ -50,11 +51,84 @@ export default function Index() {
        window.location.href='/board/introduce/Ceo';
     }
  }
+ 
+ const [styleMenus,setStyleMenus] = useState([
+      {color: 'red',backGround:''},
+      {color: 'green', backGround:''},
+      {color: 'blue', backGround:''},
+    ]);
+ const degreeMenuBackColor = 6;
+ const prefixMenuBackColor = 'linear-gradient(45deg,';
+ const baseMenuBackColor = 'black';
+ const surfixMenuBackColor = ')';
+
+ useEffect(()=>{
+  const createGradation = (order) =>{
+
+    // console.log("[index.js] createGradation is it run? "+order);
+    for( var j = 0 ; j < styleMenus.length ; j++ ){
+      var rslt = prefixMenuBackColor;
+  
+      for( var i = 1 ; i <= degreeMenuBackColor ; i++ ){
+
+        if( order == i )
+          ((index) => {
+            rslt += styleMenus[index].color;
+          })(j);
+
+        else
+          rslt += baseMenuBackColor;
+    
+        if( i != degreeMenuBackColor )
+          rslt += ',';
+
+      }
+    
+      rslt += surfixMenuBackColor;
+      // console.log("[index.js] rslt css : "+rslt);
+
+      ((index, currentRslt) => {
+
+        setStyleMenus((prevStyleMenus) => {
+          const newStyleMenus = [...prevStyleMenus];
+          newStyleMenus[index].backGround = currentRslt;
+          return newStyleMenus;
+        });
+
+      })(j, rslt);
+    
+      
+    }
+    
+  
+  }
+
+  
+  // createGradation(1);
+  let numGradation = -1;
+
+  const intervalGradation = setInterval(() => {
+    numGradation = ( (numGradation + 1) % degreeMenuBackColor );
+    createGradation(numGradation + 1);
+    // console.log("[index.js] numGradation : "+ ( numGradation + 1 ) );
+  }, 100);
+
+  return () => clearInterval(intervalGradation);
+
+  // console.log("[index.js] \n1 : "+styleMenus[0].backGround
+  // +", \n2 : "+styleMenus[1].backGround
+  // +", \n3 : "+styleMenus[2].backGround);
+
+ },[]);
+
+ function sendTestMsg(msg){
+    console.log("[index.js] msg : "+msg);
+ };
 
  function openKakao(){
   // window.location.href=process.env.NEXT_PUBLIC_REACT_APP_OPEN_KAKAO;
   window.open(process.env.NEXT_PUBLIC_REACT_APP_OPEN_KAKAO, '_blank', 'noopener,noreferrer');
- }
+ };
 
   useEffect(()=>{
 
@@ -66,6 +140,21 @@ export default function Index() {
   
 
   },);
+
+  useEffect(()=>{
+    const handleResize = () => {
+
+      setIsMobile(window.innerWidth <= mobileWidth);
+
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  },[]);
 
 
    return (
@@ -94,17 +183,70 @@ export default function Index() {
                   objectFit="contain"
                   />
              </div>
-             <div className="w-full max-w-full space-y-4 mx-auto">
+             { !isMobile ? 
+              <div className="w-full max-w-full space-y-4 mx-auto">
+                <div className="grid grid-cols-3 gap-8">
+                  <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                        onClick={()=>clickMenu(0)}>
+                    <Image src={navi_icon_1} alt='개발자 이력 소개' style={{width: '40%', height: '40%'}}/>
+                    <h2 className="text-xl font-bold text-white">개발자 이력 소개</h2>
+                    <p className="text-zinc-200 dark:text-zinc-100">
+                      개발자의 작업 경험과 간단한 이력사항 보유 기술을 보여드립니다.
+                    </p>
+                  </div>                 
+                  <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                        onClick={()=>clickMenu(1)}>
+                    <Image src={navi_icon_3} alt='즐길거리' style={{width: '40%', height: '40%'}}/>
+                    <h2 className="text-xl font-bold text-white">즐길거리</h2>
+                    <p className="text-zinc-200 dark:text-zinc-100">
+                      간단한 즐길거리, 정보 공유를 하는 곳입니다.<br/>
+                      ( 현재 준비중 )
+                    </p>
+                  </div>
+                  <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                        onClick={()=>openKakao()}>
+                    <Image src={navi_icon_4} alt='문의/제안/의뢰' style={{width: '40%', height: '40%'}}/>
+                    <h2 className="text-xl font-bold text-white">문의 및 제안</h2>
+                    <p className="text-zinc-200 dark:text-zinc-100">
+                      원하시는 문의/제안/의뢰 어떤 것도 환영합니다.<br/>
+                      지속적이고 건강한 관계를 위해 정확하게 말씀드리고 제안드립니다.
+                    </p>
+                  </div>
+                </div>
+              </div>
+             :
+              <div>
+                <div className='text-gray-100 mb-2' 
+                      onClick={()=>clickMenu(0)}
+                      style={{background: styleMenus[0].backGround}}>
+                  <a>
+                    개발자 이력 소개
+                  </a>
+                </div>
+                <div className='text-gray-100 mb-2'
+                      style={{background: styleMenus[1].backGround}}>
+                  <a>
+                    즐길거리
+                  </a>
+                </div>
+                <div className='text-gray-100 mb-2'
+                      style={{background: styleMenus[2].backGround}}>
+                  <a>
+                    문의 및 제안
+                  </a>
+                </div>
+              </div>
+             }
+             {/* <div className="w-full max-w-full space-y-4 mx-auto">
                <div className="grid grid-cols-3 gap-8">
-                 <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
+                <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
                       onClick={()=>clickMenu(0)}>
-                   <Image src={navi_icon_1} alt='개발자 이력 소개' style={{width: '40%', height: '40%'}}/>
-                   <h2 className="text-xl font-bold text-white">개발자 이력 소개</h2>
-                   <p className="text-zinc-200 dark:text-zinc-100">
-                     개발자의 작업 경험과 간단한 이력사항 보유 기술을 보여드립니다.
-                     ( 현재 준비중 )
-                   </p>
-                 </div>
+                  <Image src={navi_icon_1} alt='개발자 이력 소개' style={{width: '40%', height: '40%'}}/>
+                  <h2 className="text-xl font-bold text-white">개발자 이력 소개</h2>
+                  <p className="text-zinc-200 dark:text-zinc-100">
+                    개발자의 작업 경험과 간단한 이력사항 보유 기술을 보여드립니다.
+                  </p>
+                </div>                 
                  <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"
                       onClick={()=>clickMenu(1)}>
                    <Image src={navi_icon_3} alt='즐길거리' style={{width: '40%', height: '40%'}}/>
@@ -123,33 +265,8 @@ export default function Index() {
                      지속적이고 건강한 관계를 위해 정확하게 말씀드리고 제안드립니다.
                    </p>
                  </div>
-                 
-                 {/* <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
-                   <div className="p-2 bg-black bg-opacity-50 rounded-full">
-                     <svg
-                       className=" text-white h-6 w-6 mb-2 opacity-75"
-                       fill="none"
-                       height="24"
-                       stroke="currentColor"
-                       strokeLinecap="round"
-                       strokeLinejoin="round"
-                       strokeWidth="2"
-                       viewBox="0 0 24 24"
-                       width="24"
-                       xmlns="http://www.w3.org/2000/svg"
-                     >
-                       <path d="m8 6 4-4 4 4" />
-                       <path d="M12 2v10.3a4 4 0 0 1-1.172 2.872L4 22" />
-                       <path d="m20 22-5-5" />
-                     </svg>
-                   </div>
-                   <h2 className="text-xl font-bold text-white">Easy Collaboration</h2>
-                   <p className="text-zinc-200 dark:text-zinc-100">
-                     Easy Collaboration allows you to share and edit documents with your team in real time.
-                   </p>
-                 </div> */}
                </div>
-             </div>
+             </div> */}
 
              <div style={{display:'flex',justifyContent:'center'}}>
               <SelectComponent/>
